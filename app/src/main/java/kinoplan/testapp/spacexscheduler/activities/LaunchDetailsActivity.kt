91ecf1e5f81041831d32fa04a921269312e78c67
Mikhail <wot.smailik@yandex.ru>
2019-11-12 22:47:17 +0300

    Started new "launch details" activity

---
+++ app/src/main/java/kinoplan/testapp/spacexscheduler/activities/LaunchDetailsActivity.kt
@@ -0,0 +1,12 @@
+package kinoplan.testapp.spacexscheduler.activities
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class LaunchDetailsActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_launch_details)
+    }
+}
